673d4b93371ac81595beb3035e6d04c9ce248eff
Alejandra Agreda <agos.alejandramaria@gmail.com>
2017-04-18 20:53:29 -0400

    folder server was created to hold Backend

---
+++ app/app.js
@@ -0,0 +1,15 @@
+// Include the Main React Dependencies
+
+
+// We need to include all of the components we"re utilizing
+
+// Include the PageOne, PageTwo, PageThree, and PageFourth components
+
+//Routes 
+//API Calls go here ---server axios to connect
+//componentDidMouth()
+
+// Here we dump all of the components into a single main-container: MainPage
+
+// Again, treat them like they are any other HTML elements.
+  
