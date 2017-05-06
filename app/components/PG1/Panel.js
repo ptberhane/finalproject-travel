0fad20b16c4a82a03401e1109526899c0e60bcec
simishr <smriti.shrestha271@gmail.com>
2017-05-06 01:17:32 -0400

    side panel edit

--- app/components/PG1/Panel.js
+++ app/components/PG1/Panel.js
@@ -10,31 +10,25 @@ var Panel= React.createClass({
           <button className="#004d40 teal lighten-2 white-text"> Blogs<i className="small material-icons">library_books</i></button>
           <div id="slideout_inner" className="#263238 blue-grey darken-4">
               <ul style={styless}>
-                <li className="collection-item avatar N/A transparent z-depth-0">
-                  <div className="white-text">
-                    <p className="title">The best shoes for Travel- tips from a Podiatrist</p>
-                    <p>Stephanie Raley <br/>
-                       May 1, 2017
-                    </p>
-                  </div>
+                <li className="collection-item avatar N/A teal transparent z-depth-0">
+                  <p className="title"><i className="tiny material-icons">assignment</i>  Take the tips out of travelling</p>
+                  <h6><i className="tiny material-icons">mode_edit</i>Jane Dempster-Smith <br/>
+                   April 15, 2017
+                </h6>
                 </li>
-
-                <li className="sidePanel collection-item avatar N/A transparent z-depth-0">
-                  <div className="white-text">
-                    <p className="title">The best shoes for Travel- tips from a Podiatrist</p>
-                    <p>Stephanie Raley <br/>
-                       May 1, 2017
-                    </p>
-                  </div>
+                  <br />
+                <li className="collection-item avatar N/A teal transparent z-depth-0">
+                  <p className="title"><i className="tiny material-icons">assignment</i>  Post Cards from Maui</p>
+                  <h6><i className="tiny material-icons">mode_edit</i>Craig <br/>
+                   April 12, 2017
+                </h6>
                 </li>
-
-                <li className="sidePanel collection-item avatar N/A transparent z-depth-0">
-                  <div className="white-text">
-                    <p className="title">The best shoes for Travel- tips from a Podiatrist</p>
-                    <p>Stephanie Raley <br/>
-                       May 1, 2017
-                    </p>
-                  </div>
+                   <br />
+                <li className="collection-item avatar N/A teal transparent z-depth-0">
+                  <p className="title"><i className="tiny material-icons">assignment</i>  Does travel have to stop once you have Kids</p>
+                  <h6><i className="tiny material-icons">mode_edit</i>Kez <br/>
+                   March 28, 2017
+                </h6>
                 </li>
                 
               </ul>
@@ -45,7 +39,9 @@ var Panel= React.createClass({
 });
 const styless = {
   width: '300px',
-  border: 'none'
+  border: 'none',
+  color: '#ffffff',
+  padding: '3px'
 }
 
 module.exports= Panel;
